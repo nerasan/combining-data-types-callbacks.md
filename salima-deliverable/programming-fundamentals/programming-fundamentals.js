69daa1f2ed41625121a10700d406278fc8b53f4c
nerasan <salima.harun@gmail.com>
2020-09-14 21:20:36 -0500

    added hw answers to programming-fundamentals.js

---
+++ salima-deliverable/programming-fundamentals/programming-fundamentals.js
@@ -0,0 +1,13 @@
+  // PROGRAMMING FUNDAMENTALS -- QUESTIONS 
+  
+  // 1. In keeping with one of our programming principals (write code for the maintainer): What would have been a more semantic name for this function (hint: this is a problem we had for either lab or hw and is considered a classic ) - are there any other variable names or things that would make this code easier to read and understand?
+  
+  // it could be called the fibonacci function, similar to the hw we did. it adds the previous two numbers. the way the second code is written is much easier to understand what the variables are representing.
+  
+  // 2. If you started a new job and your project was to expand the functionality of this function by allowing a second argument and then based on that second argument, returning the sum of even or odd numbers, which code would you rather start working with f or f2?
+  
+  // i would rather start working with f2 as it is easier to update the code line by line. it is also easier to define the new variables and parameter that will be needed.
+  
+  // 3. Finally, the 'shorter' code style doesn't use semi-colons, except for the fourth line. Remove this semi-colon! Run the code, is this semi-colon necessary?
+  
+  // the semi-colon is necessary. if removed, it does not work as it gives an error of invalid left-hand side in assignment.
